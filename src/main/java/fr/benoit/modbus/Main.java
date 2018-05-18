3fd27122c8a330581bd870ec675a9d74fe8f2e95
Rapace21 <rapace21@gmail.com>
2018-05-18 09:05:33 +0200

    Project initialization

---
+++ src/main/java/fr/benoit/modbus/Main.java
@@ -0,0 +1,15 @@
+package fr.benoit.modbus;
+
+/**
+ * <br>
+ * Author : <a href="https://git-01.dev.uhcwork.net/u/Rapace">Wazo</a><br>
+ * Project modbusunite
+ * <br>
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+
+}
